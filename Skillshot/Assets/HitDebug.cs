3141983bca8f0e8aab5cef6a40a13d8c56f764ad
Plum <pplluumm@users.noreply.github.com>
2018-01-10 13:09:22 -0800

    Slash as trigger
    Slash is now trigger insted of collider, so now it will trigger as it moves through objects instead of stopping.

--- Skillshot/Assets/HitDebug.cs
+++ Skillshot/Assets/HitDebug.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class HitDebug : MonoBehaviour {
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(this.name + " hit: " + collision.transform.name);
+        Debug.Log(this.name + " collided with: " + collision.transform.name);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(this.name + " trigger hit: " + collision.transform.name);
     }
 }
